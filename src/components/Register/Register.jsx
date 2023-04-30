1b570303d388e41c2d7a695b4a4cad86bcf496fb
sabbir1257 <masabbir1257@gmail.com>
2023-04-30 23:49:12 +0600

    push

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile } from "firebase/auth";
 import app from "../../firebase/firebase.config";
+import { Link } from "react-router-dom";
 
 const auth = getAuth(app);
 
@@ -10,7 +11,7 @@ const Register = () => {
 
   const handleEmailChange = (event) => {
     console.log(event.target.value);
-    // setEmail(event.target.value);
+    setEmail(event.target.value);
   };
 
   const handleSubmit = (event) => {
@@ -20,7 +21,8 @@ const Register = () => {
     // 2. collect form data
     const email = event.target.email.value;
     const password = event.target.password.value;
-    console.log(email, password);
+    const name = event.target.name.value; 
+    console.log(name, email, password);
     // validate
     if (!/(?=.*[A-Z])/.test(password)) {
         setError('Please add at least one uppercase');
@@ -42,6 +44,8 @@ const Register = () => {
         setError('');
         event.target.reset()
         setSuccess('User has created success')
+        sendVerificationEmail(result.user);
+        updateUserData(result.user, name);
       })
       .catch((error) => {
         console.error(error.massage);
@@ -49,11 +53,39 @@ const Register = () => {
       });
   };
 
+  const sendVerificationEmail = (user) => {
+    sendEmailVerification(user)
+    .then(result => {
+      console.log(result);
+      alert('Please verify your email')
+    })
+  }
+
+  const updateUserData = (user, name) => {
+    updateProfile(user, {
+      displayName: name
+    })
+    .then( () => {
+      console.log('user name updated');
+    })
+    .catch(error => {
+      setError(error.message);
+    })
+  }
+
   const handlePasswordBlur = (event) => [console.log(event.target.value)];
   return (
-    <div>
+    <div className="w-50 mx-auto ">
       <h2>register</h2>
       <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Your N~ame"
+          required
+        />
+        <br />
         <input
           onChange={handleEmailChange}
           type="email"
@@ -74,6 +106,7 @@ const Register = () => {
         <br />
         <input type="submit" value="Register" />
       </form>
+      <p><small>Already have an account? Please <Link to="/login">Login</Link> </small></p>
       <p className="text-danger">{error}</p>
       <p className="text-success">{success}</p>
     </div>
