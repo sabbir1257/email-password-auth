3346aa1135554f04fd648008d0410ba1ce379b48
sabbir1257 <masabbir1257@gmail.com>
2023-04-30 13:01:49 +0600

    push

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,35 +1,83 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from "../../firebase/firebase.config";
+
+const auth = getAuth(app);
 
 const Register = () => {
-    const [email, setEmail] = useState('')
-    
-    const handleEmailChange = (event) => {
-        console.log(event.target.value)
-        setEmail(event.target.value)
-    }
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const email = event.target.email.value;
-        const password = event.target.password.value
-        console.log(email, password);
-    }
+  const handleEmailChange = (event) => {
+    console.log(event.target.value);
+    // setEmail(event.target.value);
+  };
 
-    const handlePasswordBlur = (event) => [
-        console.log(event.target.value)
-    ]
-    return (
-        <div>
-            <h2>register</h2>
-            <form onSubmit={handleSubmit}>
-                <input onChange={handleEmailChange } type="email" name="email" id="email" placeholder='Your Email' />
-                <br />
-                <input onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Your Password' />
-                <br />
-                <input type="submit" value="Register" />
-            </form>
-        </div>
-    );
+  const handleSubmit = (event) => {
+    // 1. prevent page refresh
+    event.preventDefault();
+    setSuccess('');
+    // 2. collect form data
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email, password);
+    // validate
+    if (!/(?=.*[A-Z])/.test(password)) {
+        setError('Please add at least one uppercase');
+        return;
+    }
+    else if (!/(?=.*[0-9].*[0-9])/.test(password)) {
+        setError('Please add at least two numbers');
+        return
+    }
+    else if (password.length < 6) {
+        setError('Please add at least 6 characters in your password')
+        return;
+    }
+    //3. create user in fb
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        setError('');
+        event.target.reset()
+        setSuccess('User has created success')
+      })
+      .catch((error) => {
+        console.error(error.massage);
+        setError(error.massage);
+      });
+  };
+
+  const handlePasswordBlur = (event) => [console.log(event.target.value)];
+  return (
+    <div>
+      <h2>register</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleEmailChange}
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Your Email"
+          required
+        />
+        <br />
+        <input
+          onBlur={handlePasswordBlur}
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Your Password"
+          required
+        />
+        <br />
+        <input type="submit" value="Register" />
+      </form>
+      <p className="text-danger">{error}</p>
+      <p className="text-success">{success}</p>
+    </div>
+  );
 };
 
 export default Register;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBS1SQQz4tSXxO8v0Cxa_qDRzPxugJwIMQ",
+  authDomain: "email-password-auth-59a0d.firebaseapp.com",
+  projectId: "email-password-auth-59a0d",
+  storageBucket: "email-password-auth-59a0d.appspot.com",
+  messagingSenderId: "1092516891872",
+  appId: "1:1092516891872:web:88b216f8beb24423fb1485"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
